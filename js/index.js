8e7c16aafb0aeb36532a5171a12c1f058bd7fe94
Oleksandr0007 <yagodinetc@gmail.com>
2022-10-20 14:00:33 +0300

    Add sign_in.html

--- js/index.js
+++ js/index.js
@@ -17,19 +17,61 @@ function downValue()
     }
 
 
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+
+    //scrol up
+    if ( prevScrollpos > currentScrollPos ) {
+        document.getElementById("asd").style.top = "0";
+        document.getElementById("asd").style.backgroundColor = "#494B4B";
+        
+        // if on start page
+        if ( window.scrollY < window.innerHeight ) {
+            //change color text on header      
+            var link = document.querySelectorAll(".main__link")
+            link.forEach(link => {
+                link.style.color = "#000";
+            });
+            document.getElementById("asd").style.backgroundColor = "#fff";
+
+            //chenge color header
+            document.getElementById("img_col").style.stroke = "#000";
+            document.getElementById("img_col1").style.fill = "#000";
+
+        } 
+    //scroll down
+    } else {
+    document.getElementById("asd").style.top = "-70px";
+    document.getElementById("asd").style.backgroundColor = "#494B4B";
+    document.getElementById("img_col").style.stroke = "#fff";
+    document.getElementById("img_col1").style.fill = "#fff";
+    
+    //change color text on header to wite if scrol down
+    var link = document.querySelectorAll(".main__link")
+    link.forEach(link => {
+        link.style.color = "#fff";
+    }
+    );
 
 
+    // if on start page
+    if ( window.scrollY < window.innerHeight ) {
+            //change color text on header to black if is uper than titel
+            var link = document.querySelectorAll(".main__link")
+            link.forEach(link => {
+                link.style.color = "#000";
+            }
+            );
 
-// window.onscroll = function() {myFunction()};
 
-// var header = document.getElementById("myHeader");
-
-// var sticky = header.offsetTop;
-
-// function myFunction() {
-//     if (window.pageYOffset > sticky) {
-//       header.classList.add("header__top");
-//     } else {
-//       header.classList.remove("header__top");
-//     }
-// }
+            //chenge color header
+            document.getElementById("asd").style.top = "0";
+            document.getElementById("asd").style.backgroundColor = "#fff";
+            document.getElementById("img_col").style.stroke = "#000";
+            document.getElementById("img_col1").style.fill = "#000";
+        } 
+    }
+    prevScrollpos = currentScrollPos;
+}
